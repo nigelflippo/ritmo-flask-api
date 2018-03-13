12a01c8e7e1e7c8402f87b6c32f197cc58bfd924
nigelflippo <flippo.nigel@gmail.com>
2018-03-12 21:53:12 -0600

    user routing and auth

--- app/models.py
+++ app/models.py
@@ -1,3 +1,75 @@
 import datetime
 from app import app, db, bcrypt
 import jwt
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    phone_number = db.Column(db.String(128), nullable=False)
+    zip_code = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    skill_level = db.Column(db.String(128), nullable=False)
+    instrument = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, first_name, last_name, username, email, phone_number, zip_code, password, skill_level, instrument):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.phone_number = phone_number
+        self.zip_code = zip_code
+        self.password = bcrypt.generate_password_hash(
+            password, app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.skill_level = skill_level
+        self.instrument = instrument
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'username': self.username,
+            'email': self.email,
+            'phone_number': self.phone_number,
+            'zip_code': self.zip_code,
+            'password': self.password,
+            'skill_level': self.skill_level,
+            'instrument': self.instrument
+        }
+
+    def encode_auth_token(self, user_id):
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=1800),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
+
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,85 @@
-from flask import request, make_response, jsonify, abort
+from flask import request, make_response, jsonify, abort, render_template
 from app import app, db, bcrypt
+from app.models import Users
 
 @app.route('/')
 def index():
-	return "Ritmo API"
+    return "Ritmo API"
+
+@app.route('/users/all', methods=['GET'])
+def get_users():
+	users = Users.query.order_by(Users.id).all()
+	return jsonify({'data': [Users.serialize(user) for user in users]})
+
+@app.route('/users/register', methods=['POST'])
+def register():
+	post_data = request.get_json()
+	email = post_data.get('email')
+	password = post_data.get('password')
+
+	if email is None or password is None:
+		responseObject = {
+			'status': 'error',
+			'message': 'Invalid input.'
+		}
+		return jsonify(responseObject), 400
+	if Users.query.filter_by(email=email).first() is not None:
+		responseObject = {
+			'status': 'error',
+			'message': 'User already exists.'
+		}
+		return jsonify(responseObject), 400
+	user = Users(
+		first_name = post_data.get('first_name'),
+		last_name = post_data.get('last_name'),
+        username = post_data.get('username'),
+		email = post_data.get('email'),
+		phone_number = post_data.get('phone_number'),
+		zip_code = post_data.get('zip_code'),
+		password = post_data.get('password'),
+		skill_level = post_data.get('skill_level'),
+		instrument = post_data.get('instrument')
+	)
+	db.session.add(user)
+	db.session.commit()
+
+	auth_token = user.encode_auth_token(user.id)
+	responseObject = {
+		'status': 'success',
+		'message': 'Successfully registered',
+		'auth_token': auth_token.decode()
+	}
+	return jsonify(responseObject), 201
+
+# @app.route('/auth/status', methods=['GET'])
+# def get_auth():
+# 	auth_header = request.headers.get('Authorization')
+# 	auth_token = auth_header.split(' ')[0]
+
+# 	if auth_token:
+# 		decoded = Users.decode_auth_token(auth_token)
+# 		if isinstance(decoded, str):
+# 			responseObject = {
+# 			'status': 'error',
+# 			'message': decoded
+# 			}
+# 			return jsonify(responseObject), 401
+# 		else:
+# 			user = Users.query.get(decoded)
+# 			responseObject = {
+# 				'status': 'success',
+# 				'data': {
+# 					'user_id': user.id,
+# 					'first_name': user.first_name,
+# 					'last_name': user.last_name,
+# 					'email': user.email,
+# 					'registered_on': user.registered_on
+# 				}
+# 			}
+# 			return jsonify(responseObject), 200
+# 	else:
+# 		responseObject = {
+# 			'status': 'error',
+# 			'message': 'Invalid token.'
+# 		}
+# 		return jsonify(responseObject), 401
